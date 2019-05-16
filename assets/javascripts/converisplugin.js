3942b401996ffce1cc2de5bfa55e58c63873ddf1
Thomas Hackl <thomas.hackl@uni-passau.de>
2019-05-16 12:58:35 +0200

    select correct element on click

--- assets/javascripts/converisplugin.js
+++ assets/javascripts/converisplugin.js
@@ -1,7 +1,16 @@
 (function ($) {
 
     $(document).on('click', 'input[name="type"]', function() {
-        $('section.typeselect').toggleClass('hidden-js');
+        $('section.typeselect').addClass('hidden-js');
+        $('#type-' + $(this).val()).removeClass('hidden-js');
+    });
+
+    $(document).on('dialog-open', function() {
+        $('.ui-datepicker').hide();
+        $('#start-date').on('click', function() {
+            $('.ui-datepicker').show();
+        });
+        $('#template').focus();
     });
 
 }(jQuery));
